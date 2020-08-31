3edc2b0ce3a07c7e427ab7f0c36e0fdd772710bc
Vanessa Stewart <vstewart1213@gmail.com>
2020-08-31 16:59:00 -0700

    Add initial jounrnal project environement files

---
+++ src/business-logic.js
@@ -0,0 +1,7 @@
+// Create a journaling website where a user can write entries including at least a title and body. 
+
+// Create Entry objects that include a method to return the number of words in the entry. 
+
+// Then, add a separate method (or methods) to return the number of vowels and consonants in each entry. 
+
+// Finally, add a method called getTeaser to return the first sentence of the entry. If the sentence is over 8 words, only display those first 8 words.
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import $ from 'jquery';
+import './business-logic.js';
+
+
+  $("#journalForm").submit(function(event){
+    event.preventDefault();
+
+    const inputtedTitle = $("#title").val(); 
+    const inputtedBody = $("#body").val(); 
+
+    $(".returnDiv").prepend("<h4>"+inputtedTitle+"</h4><hr><p class='lead'>"+inputtedBody+"</p><hr><br><br>");
+
+    $(".returnDiv").show();
+
+    $("#journalForm")[0].reset();
+  });
+});
